dc28edaa51e73afd40b9b53fec9d736f83e05b78
GUILLAUME ZIMBERLIN <zimberlinguillaume@gmail.com>
2025-06-02 15:52:15 +0200

    Quest06

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,3 +1,5 @@
+import categoryRepository from "./categoryRepository";
+
 // Some data to make the trick
 
 const categories = [
@@ -15,16 +17,22 @@ const categories = [
 
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  if (req.query.q != null) {
-    const filteredCategories = categories.filter((category) =>
-      category.name.includes(req.query.q as string),
-    );
+// const browse: RequestHandler = (req, res) => {
+//   if (req.query.q != null) {
+//     const filteredCategories = categories.filter((category) =>
+//       category.name.includes(req.query.q as string),
+//     );
 
-    res.json(filteredCategories);
-  } else {
-    res.json(categories);
-  }
+//     res.json(filteredCategories);
+//   } else {
+//     res.json(categories);
+//   }
+// };
+
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
--- server/src/modules/program/programActions.ts
+++ server/src/modules/program/programActions.ts
@@ -1,3 +1,5 @@
+import programRepository from "./programRepository";
+
 const programs = [
   {
     id: 1,
@@ -25,16 +27,22 @@ const programs = [
 
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = programs.filter((program) =>
-      program.synopsis.includes(req.query.q as string),
-    );
+// const browse: RequestHandler = (req, res) => {
+//   if (req.query.q != null) {
+//     const filteredPrograms = programs.filter((program) =>
+//       program.synopsis.includes(req.query.q as string),
+//     );
 
-    res.json(filteredPrograms);
-  } else {
-    res.json(programs);
-  }
+//     res.json(filteredPrograms);
+//   } else {
+//     res.json(programs);
+//   }
+// };
+
+const browse: RequestHandler = async (req, res) => {
+  const programsFromDB = await programRepository.readAll();
+
+  res.json(programsFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
